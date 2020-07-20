0fb8d89fe30c469584de8378fc74aef6c4d01815
Mohit <mm8085574989@gmail.com>
2020-07-20 22:40:18 +0530

    added dishes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap'
 import Menu from './components/MenuComponent'
 import './App.css';
+import { DISHES } from './shared/dishes';
 
+class App extends Component {
+ 
+ constructor(props)
+ {
+   super(props);
 
-function App() {
-  return (
+   this.state = {
+     dishes: DISHES
+   };
+ }
+  render()
+  {
+    return (
     <div>
       <Navbar dark color="primary">
         <div className="container">
@@ -15,9 +26,10 @@ function App() {
           </NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={this.state.dishes}/>
     </div> 
-  );
+   );
+  }
 }
 
 export default App;
